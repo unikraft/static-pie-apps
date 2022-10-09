03709ab8e737cba38fb07387770bb2af2a3db47b
anindya_sen <anindyasen007@gmail.com>
2022-10-09 23:21:03 +0530

    Add Zip static PIE build
    Signed-off-by: anindya_sen <anindyasen007@gmail.com>

---
+++ zip/build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+APP=zip
+DIR=${APP}30
+ARCHIVE=${DIR}.tar.gz
+URL=http://downloads.sourceforge.net/infozip/${ARCHIVE}
+
+# Clean up
+rm -rf ${ARCHIVE}
+rm -rf ${DIR}
+
+echo -n "Downloading ${APP} sources ... "
+wget -q ${URL}
+if test $? -ne 0; then
+    echo ""
+    echo "Unable to download ${APP} from ${URL}"
+    exit 1
+fi
+echo "DONE"
+
+echo ""
+echo -n "Unpacking ${APP} ... "
+tar -zxvf ${ARCHIVE}
+if test $? -ne 0; then
+    echo ""
+    echo "Unable to extract ${APP}"
+    exit 1
+fi
+echo "DONE"
+
+echo ""
+echo -n "Building ${APP} ... "
+echo ""
+pushd ${DIR} > /dev/null 2>&1 || exit 1
+
+patch -p1 < ../patches/add-static-pie-flag.patch
+make -f unix/Makefile generic
+
+popd > /dev/null 2>&1 || exit 1
+
+cp ${DIR}/${APP} .
+
+rm -rf ${DIR}
+rm -rf ${ARCHIVE}
+
+echo -n "Build complete!"
