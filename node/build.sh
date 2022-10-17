fb8effdb8ab583ccb0d938be98b7e5a3c17fe39a
Dave080302 <david.anghel@yahoo.com>
2022-10-18 01:14:27 +0300

    Update build.sh

--- node/build.sh
+++ node/build.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 NODE_VERSION=2.3.7
-NODE_URL="https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-"$NODE_VERSION".tar.bz2"
+NODE_URL="https://www.gnupg.org/ftp/gcrypt/gnupg/gnupg-$NODE_VERSION.tar.bz2"
 rm -rf gnupg-*
 LDFLAGS=--static-pie
 echo -n "Downloading Node " $NODE_VERSION  "..."
@@ -19,35 +20,35 @@ tar -xvf libksba-1.6.0.tar.bz2
 tar -xvf libgcrypt-1.10.1.tar.bz2
 echo ""
 echo -n "Downloading libgpg-error..... "
-pushd libgpg-error-1.45 > /dev/null 2>&1
+pushd libgpg-error-1.45 > /dev/null 2>&1 || exit 1
 ./configure
 make install
-popd > /dev/null 2>&1
+popd > /dev/null 2>&1 || exit 1
 echo -n "Downloading npth....."
-pushd npth1.6 > /dev/null 2>&1
+pushd npth1.6 > /dev/null 2>&1 || exit 1
 ./configure
 make install
-popd > /dev/null 2>&1
+popd > /dev/null 2>&1 || exit 1
 echo -n "Downloading libksba ....."
-pushd libksba-1.6.0 > /dev/null 2>&1
+pushd libksba-1.6.0 > /dev/null 2>&1 || exit 1
 ./configure
 make install
-popd > /dev/null 2>&1
+popd > /dev/null 2>&1 || exit 1
 echo -n "Downloading libgcrypt......"
-pushd libgcrypt-1.10.1 > /dev/null 2>&1
+pushd libgcrypt-1.10.1 > /dev/null 2>&1 || exit 1
 ./configure
 make install
-popd > /dev/null 2>&1
+popd > /dev/null 2>&1 || exit 1
 echo -n "Downloading libassuan......"
-pushd libassuan-2.5.5 > /dev/null 2>&1
+pushd libassuan-2.5.5 > /dev/null 2>&1 || exit 1
 ./configure
 make install
-popd > /dev/null 2>&1
+popd > /dev/null 2>&1 || exit 1
 echo -n" Unpacking Node " $NODE_VERSION "...."
 tar -xvf gnupg-"$NODE_VERSION".tar.bz2
 echo -n "Building Node $NODE_VERSION ..."
-pushd gnupg-$NODE_VERSION > /dev/null 2>&1
+pushd gnupg-$NODE_VERSION > /dev/null 2>&1 || exit 1
 ./configure
 make install
-popd > /dev/null 2>&1
+popd > /dev/null 2>&1 || exit 1
 rm *.html *.tar*
