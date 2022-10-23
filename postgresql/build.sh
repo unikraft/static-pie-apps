609e2845571188175dc916bdcdd1a707263587ed
geitner-max <49158289+geitner-max@users.noreply.github.com>
2022-10-23 18:08:24 +0200

    Update build.sh
    Signed-off-by: geitner-max <49158289+geitner-max@users.noreply.github.com>

--- postgresql/build.sh
+++ postgresql/build.sh
@@ -1,24 +1,20 @@
-# Cleanup directory
+#!/bin/bash
+
+# "Cleanup directory"
 rm -fr postgresql-*
 
-# Download Postgres
+# "Download Postgres"
 wget https://ftp.postgresql.org/pub/source/v15.0/postgresql-15.0.tar.gz
-echo ""
-echo -n "Unpacking postgreSQL"
+
+# "Unpacking postgreSQL"
 tar -xf postgresql-15.0.tar.gz
 
-echo ""
-echo $(pwd)
-pushd postgresql-15.0 > /dev/null 2>&1
+pushd postgresql-15.0 > /dev/null 2>&1 
 echo "Entering folder and configuring postgreSQL"
 patch src/bin/psql/Makefile < ../makefile.patch
 mkdir build
 ./configure --prefix=$(pwd)/build --without-readline --without-zlib CFLAGS='-fPIC' LDFLAGS_EX='-static-pie'
-#
-echo "CFLAGS: $CLAGS"
-echo "LDFLAGS: $LDFLAGS"
-echo "Building PostgreSQL"
-
+# "Building PostgreSQL"
 make
 popd > /dev/null 2>&1
 
