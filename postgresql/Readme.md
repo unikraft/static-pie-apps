adfc053b32d8f0777206444d3585c95d55a1d3d6
Maximilian Geitner <maximilian.geitner@outlook.de>
2022-10-23 17:13:44 +0200

    Static-pie for Psql

---
+++ postgresql/Readme.md
@@ -0,0 +1,30 @@
+# Postgres
+## Psql
+
+Use the `build.sh` script to download and build `psql`.
+The resulting `psql` binary is a static PIE executable loadable using the Unikraft ELF Loader. 
+Launching psql with run_elf_loader, it is required to launch with the `-U <username>` parameter, because Unikraft does not provide a default user to `psql`.
+
+# TODO
+
+
+- initdb
+- scripts
+- pgbench
+- pg_dump
+- pg_test_timing
+- pg_checksums
+- pgevent
+- pg_upgrade
+- pg_amcheck
+- pg_config
+- pg_resetwal
+- pg_verifybackup
+- pg_archivecleanup
+- pg_controldata
+- pg_rewind
+- pg_waldump
+- pg_basebackup
+- pg_ctl
+- pg_test_fsync
+
---
+++ postgresql/build.sh
@@ -0,0 +1,27 @@
+# Cleanup directory
+rm -fr postgresql-*
+
+# Download Postgres
+wget https://ftp.postgresql.org/pub/source/v15.0/postgresql-15.0.tar.gz
+echo ""
+echo -n "Unpacking postgreSQL"
+tar -xf postgresql-15.0.tar.gz
+
+echo ""
+echo $(pwd)
+pushd postgresql-15.0 > /dev/null 2>&1
+echo "Entering folder and configuring postgreSQL"
+patch src/bin/psql/Makefile < ../makefile.patch
+mkdir build
+./configure --prefix=$(pwd)/build --without-readline --without-zlib CFLAGS='-fPIC' LDFLAGS_EX='-static-pie'
+#
+echo "CFLAGS: $CLAGS"
+echo "LDFLAGS: $LDFLAGS"
+echo "Building PostgreSQL"
+
+make
+popd > /dev/null 2>&1
+
+ln -fn $(pwd)/postgresql-15.0/src/bin/psql/psql
+
+
