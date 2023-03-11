9c70b0807f11631bef4ab2b28275de21047e66aa
Anshul Singh <anshulspidy@gmail.com>
2023-03-11 18:53:24 +0530

    binary created
    Signed-off-by: Anshul Singh <anshulspidy@gmail.com>

--- vault/build.sh
+++ vault/build.sh
@@ -4,7 +4,7 @@
 rm -fr vault-*
 
 echo -n "Downloading Vault ... "
-wget -q https://releases.hashicorp.com/vault/1.8.1/vault_1.8.1_linux_amd64.zip
+wget -q -O vault_1.8.1_linux_amd64.zip https://releases.hashicorp.com/vault/1.8.1/vault_1.8.1_linux_amd64.zip
 echo ""
 
 echo -n "Unpacking Vault ... "
@@ -17,8 +17,7 @@ echo "Configuring Vault ... "
 
 # Add PIC/PIE compile/link flags.
 # Disable UI and telemetry to reduce binary size and avoid network requests.
-make bootstrap UI=""
-./configure --with-cc-opt="-fPIC" --with-ld-opt="-static-pie" --disable-telemetry
+CFLAGS="-fPIC" LDFLAGS="-static-pie" ./configure --with-cc-opt="-fPIC" --with-ld-opt="-static-pie" --disable-telemetry UI=""
 
 # Remove linker flag to export dynamic symbols.
 # This causes a static PIE ELF to crash.
